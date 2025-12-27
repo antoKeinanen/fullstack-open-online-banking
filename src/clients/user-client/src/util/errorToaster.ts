b9b10d01e0473165601a8e7e19aaac665f9ae8b4
Anto Keinänen <anto.keinanen@gmail.com>
2025-12-27 09:37:49 +0200

    chore: use forEach over map

--- src/clients/user-client/src/util/errorToaster.ts
+++ src/clients/user-client/src/util/errorToaster.ts
@@ -3,5 +3,5 @@ import { toast } from "sonner";
 import type { ApiError } from "./api";
 
 export function toastErrors(error: ApiError) {
-  error.errors.filter((e) => e.showUser).map((e) => toast.error(e.message));
+  error.errors.filter((e) => e.showUser).forEach((e) => toast.error(e.message));
 }
