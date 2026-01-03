7abdab07e02205bc64152cee49d6bbf137559249
Anto Keinänen <anto.keinanen@gmail.com>
2026-01-03 17:14:46 +0200

    ref: refactor stripe methods

--- src/services/user-bff/src/routes/stripe.ts
+++ src/services/user-bff/src/routes/stripe.ts
@@ -8,10 +8,18 @@ import { generateStripeCheckoutRequestSchema } from "@repo/validators/stripe";
 
 import type { JwtPayload } from "..";
 import { env } from "../env";
-import { stripe, stripeService } from "../services/stripe";
+import {
+  handleCancelled,
+  handleFailed,
+  handleProcessing,
+  handleSuccess,
+} from "../events/paymentIntent";
+import {
+  createStripeCheckout,
+  getOrCreateStripeCustomer,
+  stripeConstructWebhookEvent,
+} from "../services/stripe";
 import { userService } from "../services/userService";
-import { formatAsHex } from "../util/formatter";
-import { tryCatch } from "../util/tryCatch";
 
 export const stripeRouter = new Hono();
 
@@ -29,83 +37,42 @@ stripeRouter.post(
   async (c) => {
     const { sub: userId } = c.get("jwtPayload") as JwtPayload;
     const { amount } = c.req.valid("json");
-    const { data: stripeCustomer, error: getStripeCustomerIdError } =
-      await stripeService.getStripeCustomerId({ userId });
-    if (getStripeCustomerIdError) {
+
+    const { data: user, error: userError } = await userService.getUserById({
+      userId,
+    });
+    if (userError) {
+      console.error("Failed to get user", userError.details, userError.message);
+      return c.json(createUnexpectedError(), 500);
+    }
+
+    // THIS SHOULD NEVER HAPPEN
+    if (!user.phoneNumber) {
       console.error(
-        "Failed to get stripe customer id",
-        getStripeCustomerIdError.details,
-        getStripeCustomerIdError.message,
+        "Failed to create stripe checkout: user is missing phone number",
       );
       return c.json(createUnexpectedError(), 500);
     }
 
-    let stripeCustomerId = stripeCustomer.stripeCustomerId;
-    if (!stripeCustomerId) {
-      const { data: user, error: userError } = await userService.getUserById({
-        userId,
-      });
-      if (userError) {
-        console.error(
-          "Failed to get user",
-          userError.details,
-          userError.message,
-        );
-        return c.json(createUnexpectedError(), 500);
-      }
-
-      const newCustomer = await stripe.customers.create({
-        phone: user.phoneNumber,
-      });
-
-      const { error: setStripeCustomerIdError } =
-        await stripeService.setStripeCustomerId({
-          userId,
-          stripeCustomerId: newCustomer.id,
-        });
-
-      if (setStripeCustomerIdError) {
-        console.error(
-          "Failed to get stripe customer id",
-          setStripeCustomerIdError.details,
-          setStripeCustomerIdError.message,
-        );
-        return c.json(createUnexpectedError(), 500);
-      }
-
-      stripeCustomerId = newCustomer.id;
+    const { data: stripeCustomerId, error: stripeCustomerError } =
+      await getOrCreateStripeCustomer(user.phoneNumber, userId);
+    if (stripeCustomerError) {
+      return c.json(createUnexpectedError(), 500);
     }
 
-    const amountInCents = amount * 100;
-
-    const checkout = await stripe.checkout.sessions.create({
-      customer: stripeCustomerId,
-      success_url: env.USER_BFF_STRIPE_SUCCESS_ENDPOINT,
-      ui_mode: "hosted",
-      phone_number_collection: {
-        enabled: true,
-      },
-      billing_address_collection: "auto",
-      line_items: [
-        {
-          price_data: {
-            currency: "eur",
-            unit_amount: amountInCents,
-            product_data: {
-              name: `${amount}€ deposit`,
-              description: `Add ${amount}€ to your account`,
-            },
-          },
-          quantity: 1,
-        },
-      ],
-      mode: "payment",
-      metadata: {
-        amount: amount.toString(),
-        userId,
-      },
-      payment_method_types: ["sepa_debit", "mobilepay", "card"],
-    });
+    const { data: checkout, error: checkoutError } = await createStripeCheckout(
+      stripeCustomerId,
+      amount,
+      userId,
+    );
+    if (checkoutError) {
+      console.error(
+        "Failed to create stripe checkout",
+        checkoutError.message,
+        checkoutError.stack,
+      );
+      return c.json(createUnexpectedError(), 500);
+    }
 
     return c.json({ url: checkout.url });
   },
@@ -114,173 +81,35 @@ stripeRouter.post(
 stripeRouter.post("/webhook", async (c) => {
   const signature = c.req.header("stripe-signature");
   if (!signature) {
-    return c.text("", 400);
+    return c.text("signature missing", 400);
   }
   const body = await c.req.text();
-  const { data: event, error: eventError } = await tryCatch(
-    stripe.webhooks.constructEventAsync(
-      body,
-      signature,
-      env.USER_BFF_STRIPE_WEBHOOK_SECRET,
-    ),
+  const { data: event, error: eventError } = await stripeConstructWebhookEvent(
+    body,
+    signature,
   );
   if (eventError) {
-    console.error(
-      "Failed to parse stripe event from request body",
-      eventError.message,
-    );
-    return c.text("", 400);
+    console.error("Failed to construct stripe webhook event");
+    return c.text("bad event", 500);
   }
 
   console.log("[STRIPE] got event", event.type);
 
-  if (event.type == "payment_intent.succeeded") {
-    const paymentIntent = event.data.object;
-
-    if (!paymentIntent.customer) {
-      console.error("Could no process payment. Missing customer");
-      return c.text("", 500);
-    }
-
-    let stripeCustomerId = null;
-    if (typeof paymentIntent.customer === "string") {
-      stripeCustomerId = paymentIntent.customer;
-    } else {
-      stripeCustomerId = paymentIntent.customer.id;
-    }
-
-    const { data: pending, error: pendingError } =
-      await stripeService.getPendingTransfer({
-        stripePaymentIntentId: paymentIntent.id,
-      });
-    if (pendingError) {
-      console.error(
-        "Failed to get pending transfer",
-        pendingError.details,
-        pendingError.message,
+  switch (event.type) {
+    case "payment_intent.succeeded":
+      return handleSuccess(event, c);
+    case "payment_intent.canceled":
+      return handleCancelled(event, c);
+    case "payment_intent.payment_failed":
+      return handleFailed(event, c);
+    case "payment_intent.processing":
+      return handleProcessing(event, c);
+    default:
+      console.info(
+        "Stripe event triggered but no action was taken",
+        event.type,
       );
-      return c.text("", 500);
-    }
-
-    if (pending.tigerbeetleTransferId) {
-      const { error: transferError } = await stripeService.postPendingTransfer({
-        amount: formatAsHex(paymentIntent.amount),
-        tigerbeetleTransferId: pending.tigerbeetleTransferId,
-        userId: pending.userId,
-      });
-      if (transferError) {
-        console.error(
-          "Failed to post pending transfer",
-          transferError.message,
-          transferError.details,
-        );
-        return c.text("", 500);
-      }
-      return c.text("", 200);
-    }
-
-    const { data: user, error: userError } = await stripeService.getUserId({
-      stripeCustomerId,
-    });
-    if (userError) {
-      console.error("Failed to get user", userError.details, userError.message);
-      return c.text("", 500);
-    }
-
-    const { error: transferError } = await stripeService.createAndPostTransfer({
-      amount: formatAsHex(paymentIntent.amount),
-      userId: user.userId,
-      stripeCustomerId: stripeCustomerId,
-      stripePaymentIntentId: paymentIntent.id,
-    });
-    if (transferError) {
-      console.error(
-        "Failed to post pending transfer",
-        transferError.message,
-        transferError.details,
-      );
-      return c.text("", 500);
-    }
-  } else if (event.type == "payment_intent.processing") {
-    const paymentIntent = event.data.object;
-
-    if (!paymentIntent.customer) {
-      console.error("Could no process payment. Missing customer");
-      return c.text("", 500);
-    }
-
-    let stripeCustomerId = null;
-    if (typeof paymentIntent.customer === "string") {
-      stripeCustomerId = paymentIntent.customer;
-    } else {
-      stripeCustomerId = paymentIntent.customer.id;
-    }
-
-    const { data: user, error: userError } = await stripeService.getUserId({
-      stripeCustomerId,
-    });
-    if (userError) {
-      console.error("Failed to get user", userError.details, userError.message);
-      return c.text("", 500);
-    }
-
-    const { error: pendingError } = await stripeService.createPendingTransfer({
-      userId: user.userId,
-      amount: formatAsHex(paymentIntent.amount),
-      stripeCustomerId: stripeCustomerId,
-      stripePaymentIntentId: paymentIntent.id,
-    });
-    if (pendingError) {
-      return c.text("", 500);
-    }
-  } else if (
-    event.type == "payment_intent.canceled" ||
-    event.type == "payment_intent.payment_failed"
-  ) {
-    console.warn("Voiding payment", event.data.object.cancellation_reason);
-    const paymentIntent = event.data.object;
-
-    if (!paymentIntent.customer) {
-      console.error("Could no process payment. Missing customer");
-      return c.text("", 500);
-    }
-
-    const { data: pending, error: pendingError } =
-      await stripeService.getPendingTransfer({
-        stripePaymentIntentId: paymentIntent.id,
-      });
-    if (pendingError) {
-      console.error(
-        "Failed to get pending transfer",
-        pendingError.details,
-        pendingError.message,
-      );
-      return c.text("", 500);
-    }
-
-    if (!pending.tigerbeetleTransferId) {
-      console.error(
-        "Failed to void cancelled transfer. No transfer was found.",
-      );
-      return c.text("", 500);
-    }
-
-    const { error: transferError } = await stripeService.voidPendingTransfer({
-      amount: formatAsHex(paymentIntent.amount),
-      tigerbeetleTransferId: pending.tigerbeetleTransferId,
-      userId: pending.userId,
-    });
-    if (transferError) {
-      console.error(
-        "Failed to void pending transfer",
-        transferError.message,
-        transferError.details,
-      );
-      return c.text("", 500);
-    }
-  } else {
-    console.log("[STRIPE] unknown event", event.type);
+      return c.text("no action taken", 200);
+      break;
   }
-
-  return c.text("", 200);
 });
--- src/services/user-bff/src/services/stripe.ts
+++ src/services/user-bff/src/services/stripe.ts
@@ -3,7 +3,108 @@ import StripeSDK from "stripe";
 import { StripeService } from "@repo/service-bindings/stripe-service";
 
 import { env } from "../env";
+import { tryCatch } from "../util/tryCatch";
 
-export const stripe = new StripeSDK(env.USER_BFF_STRIPE_SECRET_KEY);
+const stripe = new StripeSDK(env.USER_BFF_STRIPE_SECRET_KEY);
 
 export const stripeService = new StripeService(env.USER_BFF_STRIPE_SERVICE_URL);
+
+export async function getOrCreateStripeCustomer(
+  phoneNumber: string,
+  userId: string,
+) {
+  const { data: customer, error: customerError } =
+    await stripeService.getStripeCustomerId({ userId: userId });
+  if (customerError) {
+    console.error(
+      "Failed to get stripe customer",
+      customerError.message,
+      customerError.details,
+    );
+    return { error: customerError };
+  }
+
+  if (customer.stripeCustomerId) {
+    return { data: customer.stripeCustomerId };
+  }
+
+  const { data: newCustomer, error: newCustomerError } = await tryCatch(
+    stripe.customers.create({
+      phone: phoneNumber,
+      metadata: {
+        userId: userId,
+      },
+    }),
+  );
+  if (newCustomerError) {
+    console.error(
+      "Failed to create a new customer",
+      newCustomerError.message,
+      newCustomerError.stack,
+    );
+    return { error: newCustomerError };
+  }
+
+  return { data: newCustomer.id };
+}
+
+export async function createStripeCheckout(
+  stripeCustomerId: string,
+  amount: number,
+  userId: string,
+) {
+  return tryCatch<StripeSDK.Checkout.Session>(
+    stripe.checkout.sessions.create({
+      customer: stripeCustomerId,
+      success_url: env.USER_BFF_STRIPE_SUCCESS_ENDPOINT,
+      ui_mode: "hosted",
+      phone_number_collection: {
+        enabled: true,
+      },
+      billing_address_collection: "auto",
+      line_items: [
+        {
+          price_data: {
+            currency: "eur",
+            unit_amount: Math.floor(amount * 100),
+            product_data: {
+              name: `${amount}€ deposit`,
+              description: `Add ${amount}€ to your account`,
+            },
+          },
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+      metadata: {
+        amount: amount.toString(),
+        userId,
+      },
+      payment_method_types: ["sepa_debit", "mobilepay", "card"],
+    }),
+  );
+}
+
+export async function stripeConstructWebhookEvent(
+  body: string,
+  signature: string,
+) {
+  return tryCatch(
+    stripe.webhooks.constructEventAsync(
+      body,
+      signature,
+      env.USER_BFF_STRIPE_WEBHOOK_SECRET,
+    ),
+  );
+}
+
+export function createStripeCustomer(phoneNumber: string, userId: string) {
+  return tryCatch<StripeSDK.Customer>(
+    stripe.customers.create({
+      phone: phoneNumber,
+      metadata: {
+        userId: userId,
+      },
+    }),
+  );
+}
